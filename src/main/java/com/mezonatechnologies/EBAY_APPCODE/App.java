9c56dce3832f15385011db6b9898d629cfe32e47
Omooyeyemi <oyeyemioahmed@gmail.com>
2023-08-22 09:04:27 +0100

    ebay-appcode

---
+++ src/main/java/com/mezonatechnologies/EBAY_APPCODE/App.java
@@ -0,0 +1,13 @@
+package com.mezonatechnologies.EBAY_APPCODE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
